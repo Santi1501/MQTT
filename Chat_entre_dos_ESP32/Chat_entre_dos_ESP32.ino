6057eb1dd46e7b6484b512f45db60e252b450c51
Santi1501 <slaurora@alumno.huergo.edu.ar>
2025-06-13 20:22:43 -0300

    Agrego ejercicio MQTT

--- Chat_entre_dos_ESP32/Chat_entre_dos_ESP32.ino
+++ Chat_entre_dos_ESP32/Chat_entre_dos_ESP32.ino
@@ -1,6 +1,10 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+WiFiClient espClient;
+// Creamos un cliente de MQTT con la conexión a internet via wifi
+PubSubClient client(espClient);
+
 // Callback (una función con una firma particular) que respondera ante un nuevo mensaje recibido
 void callback(char* topic, byte* payload, unsigned int length) {
   // Iteramos el payload
@@ -13,24 +17,30 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void setup() {
 
-  // Conectarse a una red por nombre + contraseña
-  WiFi.begin(iphone de Morci, morcilover123);
+  Serial.begin(115200);
 
-  if (WiFi.status() != WL_CONNECTED){
-    // aun no esta conectado
+  // Conectarse a una red por nombre + contraseña
+  WiFi.begin("iPhone de Morci", "morcilover123");
+
+  while (WiFi.status() != WL_CONNECTED){
+    
+    Serial.printf("\nConectando Wi-Fi");
+    delay(500);
+
   }
-  // ok
+
+  if (WiFi.status() == WL_CONNECTED){
+    
+    Serial.printf("\nWi-Fi conectado");
+
+  }
 
   // Configuramos el cliente con el callback correcto
   client.setCallback(callback);
 
-  WiFiClient espClient;
-  // Creamos un cliente de MQTT con la conexión a internet via wifi
-  PubSubClient client(espClient);
-
   // Realizamos la conexión con el servidor
-  client.setServer(broker.hivemq.com, 1883);
-  client.connect("laurogod") // devuelve un booleano indicando si se conecto correctamente
+  client.setServer("broker.hivemq.com", 1883);
+  client.connect("laurogod"); // devuelve un booleano indicando si se conecto correctamente
   client.connected(); // devuelve un booleano que nos permite chequear si la conexión fue exitosa
   client.subscribe("huergo/sistemas-embebidos/laurora"); // seguir patron del topico a escuchar
 }
@@ -41,8 +51,10 @@ void loop() {
   if (Serial.available()) {
     
     String msg = Serial.readStringUntil('\n');
+
+    String msg_final = "Laurora: "+ msg;
     // Enviar un mensaje a un chat
-    client.publish("huergo/sistemas-embebidos/laurora" , msg.c_str() ); // convertir el mensaje mediante .c_str()
+    client.publish("huergo/sistemas-embebidos/luzko" , msg_final.c_str() ); // convertir el mensaje mediante .c_str()
 
   }
   // En caso de encontrar un nuevo mensaje en el topico al que estamos subscritos, ejecutará el callback que ya seteamos
